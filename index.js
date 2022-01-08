9b21574a4d0b2d29fee1f31b6fa6ac2d9403abac
unknown <sskk8226@gmail.com>
2022-01-08 20:29:27 +0500

    lol

--- index.js
+++ index.js
@@ -1,133 +1,118 @@
-var student1 = {
-    name: "Clixicon",
-    seat_no: "CS-137",
-    result: "Pass",
-    age: 20
-}
-var student2 = {
-    name: "Scentor",
-    seat_no: "CS-175",
-    result: "Pass",
-    age: 19
-}
-var student3 = {
-    name: "Xyzwill",
-    seat_no: "CS-176",
-    result: "Pass",
-    age: 18
-}
-var student4 = {
-    name: "Backsteel",
-    seat_no: "CS-196",
-    result: "Pass",
-    age: 15
-}
-var student5 = {
-    name: "Invisible",
-    seat_no: "CS-185",
-    result: "Pass",
-    age: 17
-}
+const question = [
+    {
+        question: "What does HTML stand for?",
+        option1: "Home Tool Markup Language",
+        option2: "Hyper Text Markup Language",
+        option3: "Hyper Tool for Machine Learning",
+        option4: "None of these",
+        correctAns: "Home Tool Markup Language"
+    },
+    {
+        question: "What does CSS stand for?",
+        option1: "Cascading Style Sheets",
+        option2: "Cascade Style Sheets",
+        option3: "Covered Style Sheets",
+        option4: "None of these",
+        correctAns: "Cascading Style Sheets"
+    },
+    {
+        question: "What does JS stand for?",
+        option1: "Javascript",
+        option2: "Javasharp",
+        option3: "Javathon",
+        option4: "None of these",
+        correctAns: "Javascript"
+    },
+    {
+        question: "What does MERN stand for?",
+        option1: "Mongo DB Express React Node",
+        option2: "Manchester Express React Next",
+        option3: "Max Express Run Node",
+        option4: "None of these",
+        correctAns: "Mongo DB Express React Node"
+    },
+    {
+        question: "What does MEAN stand for",
+        option1: "Mongo DB Express Angular Node",
+        option2: "Manchester Express Anglifile Next",
+        option3: "Max Express And Node",
+        option4: "None of these",
+        correctAns: "Mongo DB Express Angular Node"
+    },
+]
 
-var student6 = {
-    name: "Spitzer",
-    seat_no: "CS-186",
-    result: "Pass",
-    age: 17
-}
-var student7 = {
-    name: "Paracha",
-    seat_no: "CS-171",
-    result: "Fail",
-    age: 25
-}
-var student8 = {
-    name: "Convict",
-    seat_no: "CS-178",
-    result: "Fail",
-    age: 24
-}
-var student9 = {
-    name: "Violent",
-    seat_no: "CS-177",
-    result: "Fail",
-    age: 21
-}
+var index = 0;
 
-var student9 = {
-    name: "Gunner12",
-    seat_no: "CS-197",
-    result: "Fail",
-    age: 24
-}
+var questCont = document.getElementById("quesCont")
+var questionBlock = document.getElementById("questionBlock")
+var resultContainer = document.getElementById("rc")
+var pgbar = document.getElementById("pgbar");
+var currentQuestion = document.getElementById("currentQuestion")
+var totalQuestion = document.getElementById("totalQuestion")
+var marks = document.getElementById("marks")
+var remarks = document.getElementById("remarks")
+var score = 0
 
-var students = [student1,student2,student3,student4,student5,student6, student7, student8, student9]
+totalQuestion.innerHTML = question.length;
 
-for (var i=0; i<students.length; i++){
-    students[i].id = i+1; 
-}
 
-var studentData = document.getElementById("studentData");
-var tbody_studentData = studentData.getElementsByTagName("tbody")[0];
-
-for (var i=0; i<students.length; i++){
-    row = tbody_studentData.insertRow();
-    cell1 = row.insertCell()
-    cell2 = row.insertCell()
-    cell3 = row.insertCell()
-    cell4 = row.insertCell()
-    cell5 = row.insertCell()
-    cell1.innerHTML = students[i].id
-    cell2.innerHTML = students[i].name
-    cell3.innerHTML = students[i].seat_no
-    cell4.innerHTML = students[i].result
-    cell5.innerHTML = students[i].age
-    if (i % 2 == 0){
-        row.setAttribute('class','table-secondary')
-    } else if (i % 2 != 0){
-        row.setAttribute('class','table-light')
-    }
-}
-
-var searchInp = document.getElementById("searchInp")
-var taBle = document.getElementById("resultBox");
-var tBody = taBle.getElementsByTagName("tbody")[0];
-
-var funcCalled = false
-
-function searchResult(){
-    var invalidID = true;
-    var iD = searchInp.value;
-    if (funcCalled == true){
-        tBody.innerHTML = "";
-    }
-    for (var i=0; i<students.length; i++){
-        if (iD == students[i].id){
-            invalidID = false;
-            console.log('inside IF')
-            var row = tBody.insertRow();
-            var cell1 = row.insertCell();
-            var cell2 = row.insertCell();
-            var cell3 = row.insertCell();
-            cell1.innerHTML = iD;
-            cell2.innerHTML = students[i].name;
-            cell3.innerHTML = students[i].result;
-            if (students[i].result == "Fail"){
-                row.setAttribute('class','table-danger')
-            } else{
-                row.setAttribute('class','table-success')
-            }
+function nextQuestion(ans) {
+    if (index <= question.length-1){
+        index++
+        if (ans.innerHTML == question[index-1].correctAns){
+            score++
+            marks.innerHTML = ((score/question.length)*100)+"%"
         }
     }
-    
-    if (invalidID){
-        alert('Invalid ID')
-    } else{
-        funcCalled = true
+    if (index < question.length) {
+        showQuestion(index);
+    } else {
+        questionBlock.style.display = "none";
+        resultContainer.style.display = "block";
     }
+    
 }
 
-function clearResult(){
-    funcCalled = false
-    tBody.innerHTML = "";
+function showQuestion(i) {
+    // console.log(index + "showquestion wala")
+    questCont.innerHTML = `
+        <div class="question rounded shadow my-4 p-4 fs-5">${question[i].question}</div>
+        <div class="options">
+            <div class="row py-2">
+                <div class="col-6">
+                    <button class="btn default-btn" onclick="nextQuestion(this)">${question[i].option1}</button>
+                </div>
+                <div class="col-6">
+                    <button class="btn default-btn" onclick="nextQuestion(this)">${question[i].option2}</button>
+                </div>
+            </div>
+            <div class="row py-2">
+                <div class="col-6">
+                    <button class="btn default-btn" onclick="nextQuestion(this)">${question[i].option3}</button>
+                </div>
+                <div class="col-6">
+                    <button class="btn default-btn" onclick="nextQuestion(this)">${question[i].option4}</button>
+                </div>
+            </div>
+        </div>
+        <div class="d-block text-end py-3">
+                <button class="btn btn-info" onclick="nextQuestion(this)" id="nextBtn">${i == question.length - 1 ? "Submit" : "Next"}</button>
+        </div>
+        <div class="progress fixed-bottom" id="p">
+            <div class="progress-bar bg-default" role="progressbar" style="width: ${((i + 1) / question.length) * 100}%" aria-valuenow="${i}"
+                aria-valuemin="0" aria-valuemax="${question.length}"></div>
+        </div>
+       `
+    currentQuestion.innerHTML = i + 1
+}
+
+showQuestion(index)
+
+var nextBtn = document.getElementById("nextBtn")
+
+var lol = document.getElementById("lol")
+
+function check(){
+    console.log(score)
+    console.log(question.length)
 }
